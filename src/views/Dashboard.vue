8da77efd7cdcee34db9416a335544703ea7290bb
OladetounJed <oladetountemitayo@gmail.com>
2020-07-10 18:13:29 +0100

    Dashboard Componenets Added

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+<template v-if="user.loggedIn">
+            <div class="nav-item">{{user.data.displayName}}</div>
+           
+          </template>   
+</div>
+     
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import fb from "../firebase";
+export default {
+     computed: {
+    ...mapGetters({
+      user: "user"
+    })
+  },
+
+}
+</script>
+
+<style>
+
+</style>
