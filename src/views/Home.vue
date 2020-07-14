a5f27f46e7a2cfe2cf63bc45c4588c706a577899
OladetounJed <oladetountemitayo@gmail.com>
2020-07-14 17:58:36 +0100

    Errors Fixed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,7 +7,7 @@
           <a href="#" class="header__logo logo animate__animated animate__bounceInDown">huntr</a>
         </div>
         <div
-          class="header__navbar-mobile animate__animated animate__bounceInDown"
+          class="header__navbar-mobile animate__animated animate__rotateInUpRight"
           v-bind:style="navStyle"
         >
           <img
